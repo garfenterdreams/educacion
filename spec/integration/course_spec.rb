garfenterdreams/educacion

commit 0ddb60ad865d0f43462377dfe705be68a5561081

show more To Do items (fixes #6140)
There is still a hard limit of 100 assignments needing grading + 100
assignments needing submitting, but that should be sufficient for most
people (I hope).

Test plan:
  - log in with a user that has a course with more than 5 assignments
    due (or that need grading)
  - click the 'N more...' link
  - see all the assignments

Change-Id: I96a3e74fc596107d849565432aa14d7706ec50c4
Reviewed-on: https://gerrit.instructure.com/8998
Tested-by: Hudson <hudson@instructure.com>
Reviewed-by: Cameron Matheson <cameron@instructure.com>

app/views/courses/_assignments_needing_grading.html.erb
@@ -4,66 +4,70 @@
 # BTW if you add a new thing here, it probably needs adding to the /users/self/todo API
 cache(safe_cache_key([@current_user, contexts, 'a_need_grading'])) do
  show_context = !contexts || contexts.length > 1
- max_to_show = 16
- if @current_user && [
-    @current_user.assignments_needing_submitting(:contexts => contexts, :limit => max_to_show),
-    @current_user.assignments_needing_grading(:contexts => contexts)[0,max_to_show],
-  ].any?{ |set| set.length > 0} %>
+ visible_limit = 5
+ needs_grading = @current_user.assignments_needing_grading(:contexts => contexts, :limit => 100)
+ needs_submitting = @current_user.assignments_needing_submitting(:contexts => contexts, :limit => 100)
+ hidden_todos = 0
+%>
+
+<% if @current_user && needs_grading.present? || needs_submitting.present? %>
   <h2><%= t('headings.to_do', %{To Do}) %></h2>
-  <% cnt = 0 %>
   <ul class="right-side-list to-do-list">
-    <% @current_user.assignments_needing_grading(:contexts => contexts)[0,max_to_show].each do |assignment| %>
-      <% cnt += 1 %>
-      <% if cnt <= 15 %>
-        <li style="<%= hidden if cnt > 5 %>">
-        <a class="item icon-grading-gray tooltip" href="<%= speed_grader_course_gradebook_path( assignment.context_id, :assignment_id => assignment.id) %>">
-            <% if assignment.due_at || assignment.points_possible || show_context %>
-              <span class="tooltip_wrap">
-                <span class='tooltip-carat'></span>
-                <span class="tooltip_text">
-                  <% if assignment.due_at %>
-                    <span style="display: block;"><%= t 'tooltip.due_at', 'due: %{date}', :date => datetime_string(assignment.due_at) %></span>
-                  <% end %>
-                  <% if assignment.points_possible %>
-                    <span style="display: block; font-size: 0.8em;"><%= t 'points_possible', 'out of %{points_possible}', :points_possible => assignment.points_possible %></span>
-                  <% end %>
-                  <% if show_context %>
-                    <span style="display: block; font-size: 0.8em;"><%= assignment.context.short_name %></span>
-                  <% end %>
-                </span>
+    <% needs_grading.each_with_index do |assignment, i| %>
+
+      <li style="<%= hidden if i >= visible_limit %>">
+      <a class="item icon-grading-gray tooltip" href="<%= speed_grader_course_gradebook_path( assignment.context_id, :assignment_id => assignment.id) %>">
+          <% if assignment.due_at || assignment.points_possible || show_context %>
+            <span class="tooltip_wrap">
+              <span class='tooltip-carat'></span>
+              <span class="tooltip_text">
+                <% if assignment.due_at %>
+                  <span style="display: block;"><%= t 'tooltip.due_at', 'due: %{date}', :date => datetime_string(assignment.due_at) %></span>
+                <% end %>
+                <% if assignment.points_possible %>
+                  <span style="display: block; font-size: 0.8em;"><%= t 'points_possible', 'out of %{points_possible}', :points_possible => assignment.points_possible %></span>
+                <% end %>
+                <% if show_context %>
+                  <span style="display: block; font-size: 0.8em;"><%= assignment.context.short_name %></span>
+                <% end %>
               </span>
-            <% end %>
-            <b><%= t 'headings.grade', 'Grade %{assignment}', :assignment => assignment.title %></b>
-            <em><%= t 'need_grading_count', { :one => '1 needs grading', :other => '%{count} need grading' }, :count => assignment.needs_grading_count %></em>
-          </a>
-          <a class='disable_item_link grading' title="<%= t('links.title.ignore', %{Ignore this assignment}) %>" href="<%= api_v1_users_todo_ignore_url(assignment.asset_string, 'grading') %>"><%= image_tag "earmark_hover.png", :alt => t('images.alt.ignore', 'ignore') %></a>
-        </li>
-      <% end %>
+            </span>
+          <% end %>
+          <b><%= t 'headings.grade', 'Grade %{assignment}', :assignment => assignment.title %></b>
+          <em><%= t 'need_grading_count', { :one => '1 needs grading', :other => '%{count} need grading' }, :count => assignment.needs_grading_count %></em>
+        </a>
+        <a class='disable_item_link grading' title="<%= t('links.title.ignore', %{Ignore this assignment}) %>" href="<%= api_v1_users_todo_ignore_url(assignment.asset_string, 'grading') %>"><%= image_tag "earmark_hover.png", :alt => t('images.alt.ignore', 'ignore') %></a>
+      </li>
     <% end %>
-    <% @current_user.assignments_needing_submitting(:contexts => contexts, :limit => max_to_show).each do |assignment| %>
-      <% cnt += 1 %>
-      <% if cnt <= 15 %>
-        <li style="<%= hidden if cnt > 5 %>">
-          <a class="item icon-grading-gray tooltip" href="<%= course_assignment_path( assignment.context_id, assignment.id ) %>#submit">
-            <% if show_context || assignment.points_possible %>
-              <span class="tooltip_wrap">
-                <span class='tooltip-carat'></span>
-                <span class="tooltip_text">
-                  <% if assignment.points_possible %>
-                    <span style="display: block; font-size: 0.8em;"><%= t 'points_possible', 'out of %{points_possible}', :points_possible => assignment.points_possible %></span>
-                  <% end %>
-                  <% if show_context %>
-                    <span style="display: block; font-size: 0.8em;"><%= assignment.context.short_name %></span>
-                  <% end %>
-                </span>
+    <% needs_submitting.each_with_index do |assignment, i| %>
+      <li style="<%= hidden if i >= visible_limit %>">
+        <a class="item icon-grading-gray tooltip" href="<%= course_assignment_path( assignment.context_id, assignment.id ) %>#submit">
+          <% if show_context || assignment.points_possible %>
+            <span class="tooltip_wrap">
+              <span class='tooltip-carat'></span>
+              <span class="tooltip_text">
+                <% if assignment.points_possible %>
+                  <span style="display: block; font-size: 0.8em;"><%= t 'points_possible', 'out of %{points_possible}', :points_possible => assignment.points_possible %></span>
+                <% end %>
+                <% if show_context %>
+                  <span style="display: block; font-size: 0.8em;"><%= assignment.context.short_name %></span>
+                <% end %>
               </span>
-            <% end %>
-            <b><%= assignment.submission_action_string %></b>
-            <em><%= t 'due_at', 'Due %{date}', :date => datetime_string(assignment.due_at) %></em>
-          </a>
-          <a class='disable_item_link submitting' title="<%= t('links.title.ignore', %{Ignore this assignment}) %>"  href="<%= api_v1_users_todo_ignore_url(assignment.asset_string, 'submitting') %>"><%= image_tag "earmark_hover.png", :alt => t('images.alt.ignore', 'ignore') %></a>
-        </li>
-      <% end %>
+            </span>
+          <% end %>
+          <b><%= assignment.submission_action_string %></b>
+          <em><%= t 'due_at', 'Due %{date}', :date => datetime_string(assignment.due_at) %></em>
+        </a>
+        <a class='disable_item_link submitting' title="<%= t('links.title.ignore', %{Ignore this assignment}) %>"  href="<%= api_v1_users_todo_ignore_url(assignment.asset_string, 'submitting') %>"><%= image_tag "earmark_hover.png", :alt => t('images.alt.ignore', 'ignore') %></a>
+      </li>
+    <% end %>
+    <% hidden_items = [needs_grading.length - visible_limit, 0].max + [needs_submitting.length - visible_limit, 0].max %>
+    <% if hidden_items > 0 %>
+      <li>
+      <a href="#" class="more_link">
+        <%= t 'links.show_more', '%{count} more...', :count => hidden_items %>
+      </a>
+      </li>
     <% end %>
   </ul>
   <% end %>

spec/selenium/dashboard_spec.rb
@@ -101,6 +101,21 @@ describe "dashboard" do
     driver.execute_script("return $('.event a .tooltip_text').text()").should match(@course.short_name)
   end
 
+  it "should limit the number of visible items in the to do list" do
+    course_with_student_logged_in
+    due_date = Time.now.utc + 2.days
+    20.times do
+      assignment_model :due_at => due_date, :course => @course, :submission_types => 'online_text_entry'
+    end
+
+    get "/"
+
+    find_all_with_jquery(".to-do-list li:visible").size.should == 5 + 1 # +1 is the see more link
+    driver.find_element(:css, ".more_link").click
+    wait_for_animations
+    find_all_with_jquery(".to-do-list li:visible").size.should == 20
+  end
+
   it "should display assignment to grade in to do list and assignments menu for a teacher" do
     course_with_teacher_logged_in
     assignment = assignment_model({:submission_types => 'online_text_entry', :course => @course})

commit bbfea1e73cbdd855cd4e2172597a2449b3733f17

fix todo bug introduced in 0ddb60ad865d0f43462377d
Test plan:
  - go to a course page without logging in

Change-Id: I185f74f41cbe547efdba41064f650b5fe7ed2e5a
Reviewed-on: https://gerrit.instructure.com/9080
Tested-by: Hudson <hudson@instructure.com>
Reviewed-by: Cody Cutrer <cody@instructure.com>

app/views/courses/_assignments_needing_grading.html.erb
@@ -5,8 +5,8 @@
 cache(safe_cache_key([@current_user, contexts, 'a_need_grading'])) do
  show_context = !contexts || contexts.length > 1
  visible_limit = 5
- needs_grading = @current_user.assignments_needing_grading(:contexts => contexts, :limit => 100)
- needs_submitting = @current_user.assignments_needing_submitting(:contexts => contexts, :limit => 100)
+ needs_grading = @current_user ? @current_user.assignments_needing_grading(:contexts => contexts, :limit => 100) : []
+ needs_submitting = @current_user ? @current_user.assignments_needing_submitting(:contexts => contexts, :limit => 100) : []
  hidden_todos = 0
 %>
 

spec/integration/course_spec.rb
@@ -1,0 +1,32 @@
+#
+# Copyright (C) 2012 Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe "course" do
+
+  # normally this would be a controller test, but there is a some code in the
+  # views that i need to not explode
+  it "should not require authorization for public courses" do
+    course(:active_all => true)
+    @course.update_attribute(:is_public, true)
+    get "/courses/#{@course.id}"
+    response.should be_success
+  end
+
+end