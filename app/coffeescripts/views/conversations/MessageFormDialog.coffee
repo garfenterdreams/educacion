garfenterdreams/educacion

commit d6830140204d58d19eb832ff7b9e914d8244946f

fix message attachments in IE 10
test plan:
* using IE 10, should be able to add attachments when
 composing a message in conversations and send the
 message successfully
* should also work in other browsers as before

closes #CNVS-13369

Change-Id: I28459ffbbd9a58751c26d69ce9c6718d3a05c304
Reviewed-on: https://gerrit.instructure.com/42093
Reviewed-by: Clay Diffrient <cdiffrient@instructure.com>
Tested-by: Jenkins <jenkins@instructure.com>
QA-Review: Jahnavi Yetukuri <jyetukuri@instructure.com>
Product-Review: James Williams  <jamesw@instructure.com>

app/coffeescripts/views/conversations/MessageFormDialog.coffee
@@ -98,6 +98,8 @@ define [
       @launchParams = _.pick(options, 'context', 'user') if options.remoteLaunch
 
       @render()
+      @appendAddAttachmentTemplate()
+
       super
       @initializeForm()
       @resizeBody()
@@ -242,7 +244,7 @@ define [
         contextView.render()
 
       @$fullDialog.on 'click', '.message-body', @handleBodyClick
-      @$fullDialog.on 'click', '.attach-file', preventDefault =>
+      @$fullDialog.on 'click', '.attach-file', =>
         @addAttachment()
       @$fullDialog.on 'click', '.attachment .remove_link', preventDefault (e) =>
         @removeAttachment($(e.currentTarget))
@@ -333,12 +335,19 @@ define [
       ($attachments.length * $attachments.outerWidth()) > @$attachmentsPane.width()
 
     addAttachment: ->
+      # when you click on the "label" that references the input it automatically open the file input
+      # we're exploiting this to get around the fact that IE won't let you submit the form when you try to
+      # "click" it through the javascript
+
+      $('#file_input').attr('id', _.uniqueId('file_input'))
+      @appendAddAttachmentTemplate()
+      @updateAttachmentOverflow()
+      @focusAddAttachment()
+
+    appendAddAttachmentTemplate: ->
       $attachment = $(addAttachmentTemplate())
       @$attachments.append($attachment)
       $attachment.hide()
-      $attachment.find('input').click()
-      @updateAttachmentOverflow()
-      @focusAddAttachment()
 
     setAttachmentClip: ($attachment) ->
       $name = $attachment.find( $('.attachment-name') )

app/views/jst/conversations/composeButtonBar.handlebars
@@ -1,6 +1,6 @@
 <div class="ui-dialog-buttonset attachments">
-  <button class="ui-button ui-widget ui-state-default ui-corner-all attach-file"
-          type="button"
+  <label for="file_input" class="ui-button ui-widget ui-state-default ui-corner-all attach-file"
+          type="button" role="button"
           title="{{#t "titles.add_attachment"}}Add an attachment{{/t}}">
     <span class="ui-button-text">
       <i class="icon-paperclip"></i>
@@ -8,7 +8,7 @@
         {{#t "titles.add_attachment"}}Add an attachment{{/t}}
       </span>
     </span>
-  </button>
+  </label>
 
   <button class="ui-button ui-widget ui-state-default ui-corner-all attach-media"
           type="button"

app/views/jst/conversations/MessageForm.handlebars
@@ -1,120 +1,0 @@
-{{#if conversation}}
-<form method="post" action="/conversations/{{conversation.id}}/add_message">
-{{else}}
-<form method="post" action="/conversations" class="new">
-{{/if}}
-  <ul class="conversations">
-    <li>
-      <span class="audience">
-        {{#if conversation}}
-          {{{audience}}}
-        {{else}}
-          {{#t "headings.new_message"}}New Message{{/t}}
-        {{/if}}
-      </span>
-      {{#if addRecipientsEnabled}}
-        <a href="/conversations/{{id}}/add_recipients" class="action_add_recipients">{{#t "inbox_actions.add_people"}}Add People{{/t}}</a>
-      {{/if}}
-    </li>
-  </ul>
-  <table>
-    {{#unless conversation}}
-    <tr class="recipient_info">
-      <th><label>{{#t "labels.to"}}To:{{/t}}</th>
-      <td><input type="text" name="recipients" data-finder_url="/conversations/find_recipients" class="recipients"></td>
-    </tr>
-    {{/unless}}
-    <tr>
-      <th><label>{{#t "labels.message"}}Message:{{/t}}</label></th>
-      <td>
-        <textarea name="body"
-                  data-track-category="Compose Message"
-                  data-track-action="Edit"
-                  data-track-label="Body"
-                  class="conversation_body">{{message}}</textarea>
-      </td>
-    </tr>
-    {{#unless conversation}}
-    <tr class="group_conversation_info" style="display:none">
-      <th></th>
-      <td>
-        <label for="group_conversation">
-          {{checkbox "group_conversation" class="group_conversation"}}
-          {{#t "labels.group_conversation"}}This is a group conversation. Participants will see everyone's replies{{/t}}
-        </label>
-      </td>
-    </tr>
-    {{/unless}}
-    <tr class="user_note_info" style="display:none">
-      <th></th>
-      <td>
-        <label for="user_note">
-          {{checkbox "user_note"
-            data-track-category="Compose Message"
-            data-track-action="Edit"
-            data-track-label="Faculty Journal"
-            class="user_note"}}
-          {{#t "labels.add_to_faculty_journal"}}Add as a Faculty Journal entry{{/t}}
-        </label>
-      </td>
-    </tr>
-    <tr>
-      <th>&nbsp;</th>
-      <td>
-        {{#if mediaCommentsEnabled}}
-          <div class="media_comment" style="display: none;" title="{{#t "titles.media_comment"}}Recorded media comment{{/t}}">
-            <img src="/images/messages/media-gray.png">
-            {{#t "media_comment"}}Media Comment{{/t}}
-            <a href="#" class="remove_link icon-end black" style="float: right;" title="{{#t "titles.remove_media_comment"}}Remove media comment{{/t}}">
-              <span class="screenreader-only">{{#t "titles.remove_media_comment"}}Remove media comment{{/t}}</span>
-            </a>
-            <div class="clear"></div>
-          </div>
-          <input type="hidden" name="media_comment_id">
-          <input type="hidden" name="media_comment_type">
-        {{/if}}
-        <ul class="attachment_list"></ul>
-        <a class="icon-paperclip action_add_attachment"
-           href="#"
-           data-track-category="Compose Message"
-           data-track-action="Edit"
-           data-track-label="Attach File"
-           title="{{#t "titles.add_attachment"}}Add an attachment{{/t}}">{{#t "links.add_attachment"}}Attach{{/t}}</a>
-        {{#if mediaCommentsEnabled}}
-          <a class="action_media_comment icon-media"
-             href="#"
-             data-track-category="Compose Message"
-             data-track-action="Edit"
-             data-track-label="Audio Recording"
-             title="{{#t "titles.record_media_comment"}}Record an audio or video comment{{/t}}"> {{#t "links.media_comment"}}Record{{/t}}</a>
-        {{/if}}
-        <div style="float: right">
-          <input type="hidden" name="from_conversation_id" value="{{from_conversation_id}}">
-          <input type="hidden" name="mode" value="async">
-          <input type="hidden" name="scope" value="{{scope}}">
-          <input type="hidden" name="filter" value="{{filter}}">
-          {{#if conversation.beta}}
-            <button type="button"
-                    data-track-category="Compose Message"
-                    data-track-action="Edit"
-                    data-track-label="ReplyAll"
-                    name="reply-type"
-                    value="all"
-                    class="btn">{{#t "buttons.reply_all"}}Reply all{{/t}}</button>
-            <button type="submit"
-                    data-track-category="Compose Message"
-                    data-track-action="Edit"
-                    data-track-label="Send"
-                    class="btn btn-primary">{{#t "buttons.reply"}}Reply{{/t}}</button>
-          {{else}}
-            <button type="submit"
-                    data-track-category="Compose Message"
-                    data-track-action="Edit"
-                    data-track-label="Send"
-                    class="btn btn-primary">{{#t "buttons.send_message"}}Send{{/t}}</button>
-          {{/if}}
-        </div>
-      </td>
-    </tr>
-  </table>
-</form>

app/views/jst/conversations/addAttachment.handlebars
@@ -12,7 +12,7 @@
       </div>
       <div class="attachment-name-clip"></div>
       <div class="attachment-name"></div>
-      <input type="file"
+      <input id="file_input" type="file"
              class="file_input"
              name="attachments[]">
   </div>

commit 6d45e00e826662a8204703426fba6b1aba047b5c

fix attachment button to work with keyboard-only
closes: CNVS-16994

this worked before but it broke in
d6830140204d58d19eb832ff7b9e914d8244946f
when we tried to get it to work in IE10

Test plan:
- Perform the following without and with a screen
  reader, in IE10, IE11, firefox, safari, & chrome
- Navigate to the compose dialog in conversations.
- Using the keyboard, tab to the "Add Attachment" button.
- Using the keyboard, verify that an attachment can in fact be attached to
  a message, and that message sent.
- Reverify the above in IE with Jaws, Firefox with NVDA, and Safari with VO.

Change-Id: Iad232a019fcb3102bb33def46e147b49d5b03b07
Reviewed-on: https://gerrit.instructure.com/46970
Tested-by: Jenkins
Reviewed-by: Andrew Butterfield <abutterfield@instructure.com>
QA-Review: Steven Shepherd <sshepherd@instructure.com>
Product-Review: Ryan Shaw <ryan@instructure.com>

app/coffeescripts/views/conversations/MessageFormDialog.coffee
@@ -147,7 +147,7 @@ define [
       @$fullDialog.addClass('compose-message-dialog')
 
       # add attachment and media buttons to bottom bar
-      @$fullDialog.find('.ui-dialog-buttonpane').prepend composeButtonBarTemplate()
+      @$fullDialog.find('.ui-dialog-buttonpane').prepend composeButtonBarTemplate({isIE10: INST.browser.ie10})
 
       @$addMediaComment = @$fullDialog.find('.attach-media')
 
@@ -338,14 +338,20 @@ define [
       ($attachments.length * $attachments.outerWidth()) > @$attachmentsPane.width()
 
     addAttachment: ->
-      # when you click on the "label" that references the input it automatically open the file input
-      # we're exploiting this to get around the fact that IE won't let you submit the form when you try to
-      # "click" it through the javascript
-
       $('#file_input').attr('id', _.uniqueId('file_input'))
       @appendAddAttachmentTemplate()
       @updateAttachmentOverflow()
-      @focusAddAttachment()
+
+      # Hacky crazyness for ie10.
+      # If you try to use javascript to 'click' on a file input element,
+      # when you go to submit the form it will give you an "access denied" error.
+      # So, for IE10, we make the paperclip icon a <label>  that references the input it automatically open the file input.
+      # But making it a <label> makes it so you can't tab to it. so for everyone else me make it a <button> and open the file
+      # input dialog with a javascript "click"
+      if INST.browser.ie10
+        @focusAddAttachment()
+      else
+        @$fullDialog.find('.file_input:last').click()
 
     appendAddAttachmentTemplate: ->
       $attachment = $(addAttachmentTemplate())

app/views/jst/conversations/composeButtonBar.handlebars
@@ -1,6 +1,7 @@
 <div class="ui-dialog-buttonset attachments">
-  <label for="file_input" class="ui-button ui-widget ui-state-default ui-corner-all attach-file"
-          type="button" role="button"
+  <{{#if isIE10}}label for="file_input" role="button"{{else}}button{{/if}}
+          class="ui-button ui-widget ui-state-default ui-corner-all attach-file"
+          type="button"
           title="{{#t "titles.add_attachment"}}Add an attachment{{/t}}">
     <span class="ui-button-text">
       <i class="icon-paperclip"></i>
@@ -8,7 +9,7 @@
         {{#t "titles.add_attachment"}}Add an attachment{{/t}}
       </span>
     </span>
-  </label>
+  </{{#if isIE10}}label{{else}}button{{/if}}>
 
   <button class="ui-button ui-widget ui-state-default ui-corner-all attach-media"
           type="button"