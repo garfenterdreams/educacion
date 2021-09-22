59cb5041eaf27f19a130db04496a36a8f088a07d
Cody Cutrer <cody@instructure.com>
2021-09-22 10:07:59 -0600

    RuboCop: Layout spec
    [skip-stages=Flakey]
    
    Change-Id: Id479617328562fd009f00db27f248c2ebafa4b7a
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/274151
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    Reviewed-by: Simon Williams <simon@instructure.com>

--- spec/messages/confirm_email_communication_channel.erb_spec.rb
+++ spec/messages/confirm_email_communication_channel.erb_spec.rb
@@ -27,7 +27,7 @@ describe 'confirm_email_communication_channel' do
     @pseudonym = @user.pseudonyms.create!(unique_id: 'unique@example.com',
                                           ***REMOVED***,
                                           password_confirmation: 'password')
-    @object = communication_channel(@user, {username: 'bob@example.com'})
+    @object = communication_channel(@user, { username: 'bob@example.com' })
   end
 
   let(:asset) { @object }
