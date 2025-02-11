garfenterdreams/educacion

commit 2227e1325551bc3cc8f9853e98908999a65ef7ce

Change use of ReactDOM.render to createRoot
flag=none
refs CFA-70

Test Plan:
- specs pass

Change-Id: Idc0d3b22ec5e56cbaf94b129b29b1d79aa73d9dc
Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/367487
Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
Reviewed-by: Drake Harper <drake.harper@instructure.com>
QA-Review: Drake Harper <drake.harper@instructure.com>
Product-Review: Drake Harper <drake.harper@instructure.com>

ui/shared/context-modules/jquery/utils.jsx
@@ -19,7 +19,7 @@
 import $ from 'jquery'
 import {some} from 'lodash'
 import React from 'react'
-import ReactDOM from 'react-dom'
+import {createRoot} from 'react-dom/client'
 import {useScope as createI18nScope} from '@canvas/i18n'
 import ModuleFile from '@canvas/files/backbone/models/ModuleFile'
 import PublishCloud from '@canvas/files/react/components/PublishCloud'
@@ -146,10 +146,8 @@ export function initPublishButton($el, data) {
       render: () => {
         const model = $el.data('view').model
 
-        ReactDOM.render(
-          <PublishCloud {...props} model={model} disabled={model.get('disabled')} />,
-          $el[0],
-        )
+        const root = createRoot($el[0])
+        root.render(<PublishCloud {...props} model={model} disabled={model.get('disabled')} />)
         // to look disable, we need to add the class here
         $el[0].classList[model.get('disabled') ? 'add' : 'remove']('disabled')
       },
@@ -555,7 +553,8 @@ function setExternalToolTray(tool, moduleData, placement = 'module_index_menu', 
     }
   }
 
-  ReactDOM.render(
+  const root = createRoot($('#external-tool-mount-point')[0])
+  root.render(
     <ContentTypeExternalToolTray
       tool={tool}
       placement={placement}
@@ -566,7 +565,6 @@ function setExternalToolTray(tool, moduleData, placement = 'module_index_menu', 
       onDismiss={handleDismiss}
       open={tool !== null}
     />,
-    $('#external-tool-mount-point')[0],
   )
 }
 
@@ -588,7 +586,8 @@ function setExternalToolModal({
     returnFocusTo.focus()
   }
 
-  ReactDOM.render(
+  const root = createRoot($('#external-tool-mount-point')[0])
+  root.render(
     <ExternalToolModalLauncher
       tool={tool}
       launchType={launchType}
@@ -599,7 +598,6 @@ function setExternalToolModal({
       onRequestClose={handleDismiss}
       contextModuleId={contextModuleId}
     />,
-    $('#external-tool-mount-point')[0],
   )
 }
 

commit 61f627bd5d83e0ded682989927af1c2d67adaf94

Unmount LTI Modal on close
This commit fixes the issue where the LTI tool modal would not close
properly when the user clicked the close button. The issue was that the
modal was not being unmounted when the close button was clicked.

This issue was introduced in 2227e1325551bc3cc8f9853e98908999a65ef7ce

test plan:

- Launch an LTI tool from the `module_index_menu` placement and the
`module_index_menu_modal` placement
- Click the close button on the LTI tool modal
- Verify that the modal closes properly

fixes INTEROP-9118
flag=none

Change-Id: I39d6297731b5e4dc3248f8b6f64ce4d085301435
Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/369148
Reviewed-by: Ryan Hawkins <ryan.hawkins@instructure.com>
QA-Review: Ryan Hawkins <ryan.hawkins@instructure.com>
Product-Review: Alexis Nast <alexis.nast@instructure.com>
Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

ui/shared/context-modules/jquery/utils.jsx
@@ -525,7 +525,7 @@ export function openExternalTool(ev) {
     setExternalToolModal({
       tool,
       launchType,
-      returnFocusTo: $('.al-trigger')[0],
+      returnFocusTo: $(`#context_module_${currentModuleId} .al-trigger`)[0],
       contextModuleId: currentModuleId,
     })
     return
@@ -548,6 +548,14 @@ export function openExternalTool(ev) {
   setExternalToolTray(tool, moduleData, launchType, $('.al-trigger')[0])
 }
 
+let externalToolRoot = null
+const getExternalToolRoot = function() {
+  if (!externalToolRoot) {
+    externalToolRoot = createRoot($('#external-tool-mount-point')[0])
+  }
+  return externalToolRoot
+}
+
 function setExternalToolTray(tool, moduleData, placement = 'module_index_menu', returnFocusTo) {
   const handleDismiss = () => {
     setExternalToolTray(null)
@@ -557,7 +565,7 @@ function setExternalToolTray(tool, moduleData, placement = 'module_index_menu', 
     }
   }
 
-  const root = createRoot($('#external-tool-mount-point')[0])
+  const root = getExternalToolRoot()
   root.render(
     <ContentTypeExternalToolTray
       tool={tool}
@@ -590,7 +598,7 @@ function setExternalToolModal({
     returnFocusTo.focus()
   }
 
-  const root = createRoot($('#external-tool-mount-point')[0])
+  const root = getExternalToolRoot()
   root.render(
     <ExternalToolModalLauncher
       tool={tool}